443b14c58c2f71ac223dbe3f9674206fd890bc6d
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-11 00:44:02 +0300

    Add base module for database interactions, importing necessary models for user, course, class session, and attendance.

---
+++ app/db/base.py
@@ -0,0 +1,5 @@
+from .session import Base
+from ..models.user import User
+from ..models.course import Course
+from ..models.class_session import ClassSession
+from ..models.attendance import Attendance
