5d4a78ec31af6317ed146dd17f21a93dbe8eaf84
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-11 00:42:53 +0300

    Add initial project structure with necessary directories and .gitignore; update requirements.txt to include python-dotenv

---
+++ app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .user import UserCreate, UserOut, UserRole
+from .token import Token, TokenData
+from .course import CourseOut
+from .class_session import ClassSessionOut
+from .attendance import AttendanceMark, AttendanceOut
